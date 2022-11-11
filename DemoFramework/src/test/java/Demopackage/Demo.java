ad80172cdfa2c0284cb88c619e818de14226f5b1
manua7022 <manuarya7022@gmail.com>
2022-11-11 12:29:13 +0530

    comitted

---
+++ DemoFramework/src/test/java/Demopackage/Demo.java
@@ -0,0 +1,33 @@
+package Demopackage;
+
+public class Demo
+{
+	
+	
+		public static void main(String[] args)
+		{
+			int[]arr= {2,1,4,3,2};
+			int[] brr= {4,6,2,7,2};
+			int length=arr.length;
+			/*if(arr.length<brr.length)
+			{
+				length=brr.length;
+			}*/
+			for(int i=0; i<length; i++)
+			{
+				
+					System.out.print(arr[i]+brr[i]+" ");
+				}
+				
+				}
+			}
+			
+		
+
+
+
+
+
+
+
+
